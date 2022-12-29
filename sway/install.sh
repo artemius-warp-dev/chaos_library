fe38160536e89c60f37ecb2e5af49d9d70d26e7f
a.salagaev <a.salagaev@fun-box.ru>
2022-12-29 21:18:29 +0300

    polkit auth has been added

--- sway/install.sh
+++ sway/install.sh
@@ -1,3 +1,4 @@
 apt isntall grim && apt install slurp swaylock swayiddle imagemagick
 
 sudo usermod -aG video ${USER}
+sudo apt-get -y install lxpolki
--- sway/wallpaper_changer.sh
+++ sway/wallpaper_changer.sh
@@ -5,7 +5,7 @@ while true; do
     sleep 300
     swaybg -i $(find /usr/share/backgrounds/. -type f | shuf -n1) -m fill &
     NEXT_PID=$!
-    sleep 5
+    sleep 200
     kill $OLD_PID
     OLD_PID=$NEXT_PID
 done
